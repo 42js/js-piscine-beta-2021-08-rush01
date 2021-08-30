ef21b2b4705499d0a82c21d520ccd3c7e4e173b2
Hyeonji-Jung <welloff.jj@gmail.com>
2021-08-30 14:39:20 +0900

    db

--- back/app.js
+++ back/app.js
@@ -6,5 +6,21 @@ const cookieParser = require('cookie-parser');
 const sequelize = require('sequelize');
 require('dotenv').config();
 const mysql = require('mysql');
+const router = express.Router();
 
 const app = express();
+
+
+
+sequelize.authenticate().then((res)=>{console.log("connected");} ).catch((err)=>{
+    console.error(err);
+})
+
+const driver = () => {
+    sequelize.sync().then(() => {
+        console.log('초기화 완료.');
+    }).catch((err) => {
+        console.error('초기화 실패');
+        console.error(err);
+    });
+};
