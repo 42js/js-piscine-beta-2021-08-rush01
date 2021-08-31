70d16a8d66f9272b56ab8b2fec30569535d8aee6
from97 <skm1104@daum.net>
2021-08-31 15:38:12 +0900

    NewRoomForm 완성

--- rush01-front/src/components/NewRoomForm/NewRoomForm.js
+++ rush01-front/src/components/NewRoomForm/NewRoomForm.js
@@ -1,8 +1,24 @@
 import React from "react";
-import NewRoomFormStyled from "./NewRoomForm.styles";
+import { NewRoomFormStyled, FormItemBox, FormItem } from "./NewRoomForm.styles";
 
 function NewRoomForm() {
-  return <NewRoomFormStyled>New Room</NewRoomFormStyled>;
+  return (
+    <NewRoomFormStyled>
+      <h2>방 생성</h2>
+      <FormItemBox>
+        <FormItem>
+          <FormItem.Title>턴 시간</FormItem.Title>
+          <FormItem.Input type="number" />초
+        </FormItem>
+        <FormItem>
+          <FormItem.Title>플레이어 당 칩 갯수</FormItem.Title>
+          <FormItem.Input type="number" />개
+        </FormItem>
+      </FormItemBox>
+      <div className="description">턴 시간 10~30초, 칩 갯수 30~50개 추천</div>
+      <button type="button">방 생성하기</button>
+    </NewRoomFormStyled>
+  );
 }
 
 export default NewRoomForm;
--- rush01-front/src/components/NewRoomForm/NewRoomForm.styles.js
+++ rush01-front/src/components/NewRoomForm/NewRoomForm.styles.js
@@ -1,5 +1,60 @@
 import styled from "styled-components";
 
-const NewRoomFormStyled = styled.div``;
+export const NewRoomFormStyled = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+width: 70%;
+height: 80%;
+font-family: 'DOSGothic';
+.description {
+    text-align: center;
+    color: #555;
+}
+h2 {
+    margin: 0 0 25px 0;
+    text-align: center;
+  }
 
-export default NewRoomFormStyled;
+  button {
+    width: 150px;
+    padding: 15px 0;
+    margin: 35px 0 0 0;
+    text-align: center;
+    border: 1px solid #ddd;
+    border-radius: 25px;
+    letter-spacing: 0.09em;
+    :hover {
+      background-color: #343a3f;
+      color: #fff;
+  }
+
+  .description {
+      text-align: center;
+      margin-bottom: 10px;
+  }
+`;
+
+export const FormItemBox = styled.div`
+  margin: 30px 0 10px 0;
+`;
+
+export const FormItem = styled.div`
+  margin-bottom: 15px;
+  border: 1px solid grey;
+  border-radius: 5px;
+  padding: 10px;
+  input {
+    width: 50px;
+    border-bottom: 1px dotted #555;
+    margin-right: 10px;
+  }
+`;
+
+FormItem.Title = styled.label`
+  display: inline-block;
+  width: 160px;
+  margin-right: 20px;
+`;
+FormItem.Input = styled.input``;
