4b4504e522e8e033c4a77051d92353ace84f340f
윤현지 <hyoon@student.42seoul.kr>
2021-08-31 22:35:06 +0900

    update Game.js

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -3,36 +3,39 @@ import axios from "axios";
 import './Game.css';
 import LoginError from './LoginError';
 import GameContent from './GameContent';
-import { Grid, Box } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import Channel from './Channel';
 import Menu from './Menu';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function Game(){
-  function Game() {
     const fetchUser = async (e) => {
-        const a = await axios.get("http://localhost:4242/login/success", {
-            withCredentials: true,
-        });
-        if (a) console.log(a);
+        try{
+            const a = await axios.get("http://localhost:4242/login/success", {
+                withCredentials: true,
+            });
+            if (a) console.log(a);
+        }catch(error){
+            console.log(error);
+            return (<LoginError />);
+        }
     };
 
     fetchUser();
-        return (
-            <BrowserRouter>
-                <div className='main' align='left'>
-                    <h1 className='header'>INDIAN POKER GAME</h1>
-                    <Grid className='grid'>
-                        <Menu className='menu' />
-                        <Switch>
-                            <Route exact path='/game'><GameContent className='content' /></Route>
-                            <Route path='/game/0:order'><Channel /></Route>
-                        </Switch>
-                    </Grid>
-                </div>
-            </BrowserRouter>
-        );
-    }
+    return (
+        <BrowserRouter>
+            <div className='main' align='left'>
+                <h1 className='header'>INDIAN POKER GAME</h1>
+                <Grid className='grid'>
+                    <Menu className='menu' />
+                    <Switch>
+                        <Route exact path='/game'><GameContent className='content' /></Route>
+                        <Route path='/game/0:order'><Channel /></Route>
+                    </Switch>
+                </Grid>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default Game;
