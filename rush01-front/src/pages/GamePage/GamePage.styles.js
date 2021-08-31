cb4d3f6ed2a697bbbee0564382335b071a950751
Kimhannah <skqkdldhf98@naver.com>
2021-08-31 17:48:37 +0900

    modify game page style

--- rush01-front/src/pages/GamePage/GamePage.styles.js
+++ rush01-front/src/pages/GamePage/GamePage.styles.js
@@ -33,8 +33,8 @@ PlayerCards.Box = styled.div`
 `;
 
 PlayerCards.Card = styled.div`
-  width: 100px;
-  height: 100px;
+  width: 150px;
+  height: 200px;
   justify-content: center;
   align-items: center;
   background: #eee;
